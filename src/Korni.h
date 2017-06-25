59da0e55368543c92d92dd351fb689a5e37ab63f
kosovanastusha <kosova_n98@mail.ru>
2017-06-26 07:30:51 +1400

    Update Korni.h

--- src/Korni.h
+++ src/Korni.h
@@ -3,4 +3,10 @@
 
 void korni(float a, float b, float c, float D);
 
+float Dec(float a,float b,float c);
+
+int Proveryaem(float D);
+
+float Znach (float D, float a, float b, float c, int i);
+
 #endif
