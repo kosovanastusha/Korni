a8c10e5f04df64ace9042c0082773e690f2b5d9e
kosovanastusha <kosova_n98@mail.ru>
2017-06-26 07:24:33 +1400

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -11,6 +11,34 @@ int main()
 	scanf("%f",&c);
 	D = b*b-4*a*c;
 	korni(a,b,c,D);
+if ( (a == 0) && (b == 0)) {
+		printf("Has no roots");
+		return 0;
+	}
+	if (a == 0) {
+		x = c * -1 / b;
+		printf ("%.2f",x);
+		return 0;
+	}
+	D = Discriment(a,b,c);
+	main = Proveryaem(D);
+	switch (i) {
+		case 0:
+			x = Znach (D, a, b, c, 1);
+			printf ("%.2f",x);
+			break;
+		case 1:
+			printf("Net korney");
+			break;
+		case 2:
+			x = Znach (D, a, b, c, 1);
+			printf ("%.2f\n",x);
+			x = Znach (D, a, b, c, -1);
+			printf ("%.2f",x);
+			break;
+	}
+	return 0;
+}
 	system("PAUSE");
 return 0;
 }
